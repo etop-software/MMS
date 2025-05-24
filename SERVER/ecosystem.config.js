1c333c7656062d3f3d2b1204fb280db9cbc12fd5
Abhishek <164993967+etop-software@users.noreply.github.com>
2025-05-24 11:25:07 +0400

    Update ecosystem.config.js
    env changes

--- SERVER/ecosystem.config.js
+++ SERVER/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "ADNH-SER",
     script: "src/index.js",  // change if your main file is different
     env: {
-      DATABASE_URL: "postgresql://etop:eTOP%40123@147.93.104.86:5432/abnhdb"
+      DATABASE_URL: "postgresql://etop:eTOP%40123@147.93.104.86:5432/mms"
     }
   }]
 }
